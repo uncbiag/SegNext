6aef2b28156eb66be14ef1c4242a373911ea5664
Qin Liu <qinliu2020@gmail.com>
2024-03-29 17:02:56 -0400

    Refactor the code

--- run_demo.sh
+++ run_demo.sh
@@ -1,4 +1,3 @@
-
 MODEL_ROOT=./weights
 MODEL_PATH=${MODEL_ROOT}/vitb_sa2_cocolvis_hq44k_epoch_0.pth
 
--- run_train.sh
+++ run_train.sh
@@ -1,11 +1,7 @@
-
-# MODEL_CONFIG=./segnext/models/default/plainvit_base1024_hqseg44k_sax2.py
-# torchrun --nproc-per-node=4 \
-# 	     --master-port 29504 \
-# 	     ./segnext/train.py ${MODEL_CONFIG} \
-# 	     --weights ./weights/vitb_sa2_cocolvis_epoch_90.pth \
-# 	     --batch-size=12 \
-# 	     --gpus=0,1,2,3
-
-MODEL_CONFIG=./segnext/models/default/plainvit_base1024_cocolvis_sax2.py
-torchrun --nproc-per-node=4 --master-port 29505 ./segnext/train.py ${MODEL_CONFIG} --batch-size=16 --gpus=0,1,2,3
+MODEL_CONFIG=./segnext/models/default/plainvit_base1024_hqseg44k_sax2.py
+torchrun --nproc-per-node=4 \
+	     --master-port 29504 \
+	     ./segnext/train.py ${MODEL_CONFIG} \
+	     --weights ./weights/vitb_sa2_cocolvis_epoch_90.pth \
+	     --batch-size=12 \
+	     --gpus=0,1,2,3
