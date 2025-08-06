3138f56401936512a37b12751d77a4f6ebf6a983
Qin Liu <qinliu2020@gmail.com>
2025-08-05 18:58:43 -0700

    Update download.py

--- download.py
+++ download.py
@@ -6,21 +6,21 @@ save_folder ='weights'
 os.makedirs(save_folder, exist_ok=True)
 
 # weights for vitb_sax1 (trained on cocolvis)
-weights_sa1_url = 'https://drive.google.com/uc?id=1eqkd5-J9MELGIw2WRcT5hejsnGc5oO30'
+weights_sa1_url = 'https://drive.google.com/uc?id=1G_m9_sVYVD2MBDf5ulrAUz-iSvi8bQfZ'
 output = f'{save_folder}/vitb_sa1_cocolvis_epoch_90.pth'
 gdown.download(weights_sa1_url, output, quiet=False)
 
 # weights for vitb_sax2 (trained on cocolvis)
-weights_sa2_url = 'https://drive.google.com/uc?id=1oxwCm4bFby6RgltO_tl54BqRN9tojylT'
+weights_sa2_url = 'https://drive.google.com/uc?id=1uHEcvwA2spsLQqDJiYKqv1pf3vgLAG7-'
 output = f'{save_folder}/vitb_sa2_cocolvis_epoch_90.pth'
 gdown.download(weights_sa2_url, output, quiet=False)
 
 # weights for vitb_sax2_ft (trained on cocolvis and finetuned on hqseg-44k)
-weights_sa2_ft_url = 'https://drive.google.com/uc?id=1yDN3mwBBO33TlA0KRdO2s07Q5HWXR6nt'
+weights_sa2_ft_url = 'https://drive.google.com/uc?id=1qkXIvxnlY3Z4xLYGWJcoAHBUNyDmvwPS'
 output = f'{save_folder}/vitb_sa2_cocolvis_hq44k_epoch_0.pth'
 gdown.download(weights_sa2_ft_url, output, quiet=False)
 
 # weights for mae-pretrained vitb (only required for training)
-weights_mae_vitb_url = 'https://drive.google.com/uc?id=1UFa-YHKSZQBrLla0za1fqfoK3QmCee3O'
+weights_mae_vitb_url = 'https://drive.google.com/uc?id=1abaho6lal3Yxthbtw2U-wEshnoPZpR-K'
 output = f'{save_folder}/mae_pretrain_vit_base.pth'
 gdown.download(weights_mae_vitb_url, output, quiet=False)
